2f18321fced48e7473fadb8f9470a069cbab3703
Ben Uni <benuni@DeShawn.(none)>
2013-02-22 22:28:45 +0000

    added options for commit

--- git_repository.rb
+++ git_repository.rb
@@ -2,7 +2,12 @@ class GitRepository
 	def initialize(system_wrapper)
 		@system_wrapper = system_wrapper
 	end
-	def commit(message)
-		@system_wrapper.execute("git commit -m '#{message}'")
+	def commit(hash)
+		if hash.has_key?(:options)
+			commit_message = "git commit #{hash[:options]} -m '#{hash[:message]}'"
+		else
+			commit_message = "git commit -m '#{hash[:message]}'"
+		end
+		@system_wrapper.execute(commit_message)
 	end
 end
--- test/test_git_repository_commit.rb
+++ test/test_git_repository_commit.rb
@@ -1,12 +1,22 @@
 require 'test/unit'
 require_relative '../git_repository'
+
 class GitRepositoryCommit < Test::Unit::TestCase
   def test_system_called_with_correct_git_message
     mock_system = MockSystemWrapper.new
     git = GitRepository.new(mock_system)
     commit_message = "this is a test message"
-    git.commit(commit_message)
+    git.commit(message: commit_message)
     assert_equal("git commit -m '#{commit_message}'",mock_system.executed_command )
+  end
+
+    def test_system_called_with_correct_git_message_and_options
+    mock_system = MockSystemWrapper.new
+    git = GitRepository.new(mock_system)
+    commit_message = "this is a test message"
+    commit_options = "-v"
+    git.commit(message: commit_message, options: commit_options)
+    assert_equal("git commit #{commit_options} -m '#{commit_message}'",mock_system.executed_command )
   end
 end
 
