b540810a51e80f2c8dcd3d344adf4b76de8fd845
Ben Flowers <ben@DeWayne.(none)>
2013-02-23 09:55:11 +0000

    added unversioned files

---
+++ rakefile.rb
@@ -0,0 +1,17 @@
+require 'git_repository'
+
+task :test_commit, :message do |t, args|
+	Rake::Task[:run_tests].invoke
+	commit(args.message)
+end
+
+task :run_tests do
+	Dir.glob("test/*.rb") do |file|
+		sh "ruby '#{file}'"
+	end
+end
+
+def commit(message)
+	git = GitRepository.new
+	git.commit(:message => message, :options => "-a")
+end
