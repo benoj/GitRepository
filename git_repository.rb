3535ad8ace446e76dfef2986edd8c7f5ff5ebcf5
Ben Flowers <ben@DeWayne.(none)>
2013-02-24 11:31:39 +0000

    refactored solution

--- git_repository.rb
+++ git_repository.rb
@@ -2,36 +2,37 @@ class GitRepository
 	def initialize(hash = {})
 		@system_wrapper = hash[:system] || SystemWrapper.new
 		@remote = hash[:remote] || 'origin'
-		@ssh_repository = hash[:ssh_repository]
+		@location = get_repository(hash[:ssh_repository]) || @remote
 	end
 	def commit(hash)
-		message_with_options = insert_options(:message => "git commit", :options=>hash[:options])
+		message_with_options = insert_options(:message => 'git commit', :options=>hash[:options])
 		commit_message = "#{message_with_options} -m '#{hash[:message]}'"
 		@system_wrapper.execute(commit_message)
 	end
 
 	def has_changes?
-		git_status = @system_wrapper.execute("git status")
-		result = git_status.include?("Changes not staged for commit") || git_status.include?("Changes to be committed:")
+		git_status = @system_wrapper.execute('git status')
+		result = git_status.include?('Changes not staged for commit') || git_status.include?('Changes to be committed:')
 		return result
 	end
 
 	def has_untracked?
 		git_status = @system_wrapper.execute("git status")
-		result = git_status.include?("Untracked files:")
-		return result
+		return git_status.include?("Untracked files:")
 	end
 
 	def push(hash = {})
 		branch = hash[:branch] || 'master'
-		repository = "--repo='#{@ssh_repository}'" unless @ssh_repository.nil?
-		location = repository || @remote
-		message_with_options = insert_options(:message => "git push", :options => hash[:options])
-		push_message = "#{message_with_options} #{location} #{branch}"
+		push_message_with_options = insert_options(:message => 'git push', :options => hash[:options])
+		push_message = "#{push_message_with_options} #{@location} #{branch}"
 		@system_wrapper.execute(push_message)
 	end
 
 	private
+	def get_repository(repository)
+		return "--repo='#{repository}'" unless repository.nil?
+	end
+
 	def insert_options(hash)
 		if(hash[:options].nil?)
 			return "#{hash[:message]}"
@@ -44,7 +45,6 @@ end
 
 class SystemWrapper
 	def execute(command)
-		result = `#{command}`
-		return result
+		`#{command}`
 	end
 end
